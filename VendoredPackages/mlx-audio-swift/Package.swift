dcc99d78bfbab325e6303819a8f2c1d78c84359a
Zheng Shao <axot@msn.com>
2026-03-19 17:33:35 +0900

    chore: vendor mlx-audio-swift as local package with patched source

---
+++ VendoredPackages/mlx-audio-swift/Package.swift
@@ -0,0 +1,56 @@
+// swift-tools-version:6.2
+import PackageDescription
+
+let package = Package(
+    name: "MLXAudio",
+    platforms: [.macOS(.v14), .iOS(.v17)],
+    products: [
+        .library(name: "MLXAudioCore", targets: ["MLXAudioCore"]),
+        .library(name: "MLXAudioSTT", targets: ["MLXAudioSTT"]),
+    ],
+    dependencies: [
+        .package(url: "https://github.com/ml-explore/mlx-swift.git", .upToNextMajor(from: "0.30.6")),
+        .package(url: "https://github.com/ml-explore/mlx-swift-lm.git", .upToNextMajor(from: "2.30.3")),
+        .package(url: "https://github.com/huggingface/swift-transformers.git", .upToNextMajor(from: "1.1.6")),
+        .package(url: "https://github.com/huggingface/swift-huggingface.git", .upToNextMajor(from: "0.6.0"))
+    ],
+    targets: [
+        .target(
+            name: "MLXAudioCore",
+            dependencies: [
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "HuggingFace", package: "swift-huggingface"),
+            ],
+            path: "Sources/MLXAudioCore",
+            swiftSettings: [
+                .unsafeFlags(["-Xfrontend", "-warn-concurrency"], .when(configuration: .debug))
+            ]
+        ),
+        .target(
+            name: "MLXAudioCodecs",
+            dependencies: [
+                "MLXAudioCore",
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
+                .product(name: "HuggingFace", package: "swift-huggingface"),
+            ],
+            path: "Sources/MLXAudioCodecs"
+        ),
+        .target(
+            name: "MLXAudioSTT",
+            dependencies: [
+                "MLXAudioCore",
+                "MLXAudioCodecs",
+                .product(name: "MLX", package: "mlx-swift"),
+                .product(name: "MLXNN", package: "mlx-swift"),
+                .product(name: "MLXLMCommon", package: "mlx-swift-lm"),
+                .product(name: "MLXLLM", package: "mlx-swift-lm"),
+                .product(name: "HuggingFace", package: "swift-huggingface"),
+                .product(name: "Transformers", package: "swift-transformers"),
+            ],
+            path: "Sources/MLXAudioSTT"
+        ),
+    ]
+)
